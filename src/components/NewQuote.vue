c7ea637ef037b13dd4a7d0a6e57a22c4b0dcc0d6
Wambui Njoroge <christinenjoroge93@gmail.com>
2019-10-04 18:11:23 +0300

    added navigation guards to new quote

--- src/components/NewQuote.vue
+++ src/components/NewQuote.vue
@@ -1,10 +1,25 @@
 <template>
-    <div>
-        <p>{{fy.atu}},This is my first quote!</p>
-        <br>
-        <br>
-        <br>
-        <router-link :to="{name:'HelloWorld'}">Back to Hello World</router-link>
+    <div id="app">
+        <h1>My Vue Router Navigation Guards Explained</h1>
+
+
+        <nav class="mainNav">
+            <!-- use router-link component for navigation. -->
+            <router-link :to="{ name: 'home'}">Home</router-link>
+            <!-- specify the link by passing the `to` prop. -->
+<!--            <router-link :to="{ name: 'user', params: { userId: 1} }">User One</router-link>-->
+<!--            <router-link :to="{ name: 'user', params: { userId: 2} }">User Two</router-link>-->
+<!--            <router-link :to="{ name: 'user', params: { userId: 3} }">User Three</router-link>-->
+<!--            <router-link :to="{ name: 'bar'}">Bar</router-link>-->
+<!--            <router-link :to="{ name: 'foo'}">Foo</router-link>-->
+            <router-link :to="{ name: 'error'}">Error</router-link>
+        </nav>
+        <!-- route outlet -->
+        <!-- component matched by the route will render here -->
+        <section class="mainBody">
+            <router-view></router-view>
+        </section>
+
     </div>
 </template>
 
@@ -14,9 +29,7 @@
         name: "NewQuote",
         data:function () {
             return{
-                fy:{
-                    atu:"Hello"
-                }
+
             }
         }
     }
--- src/router.js
+++ src/router.js
@@ -5,7 +5,14 @@ import HelloWorld from "./components/HelloWorld";
 import Wrapper from "./layouts/Wrapper";
 
 Vue.use(Router);
-
+const Err = {
+    template: `
+    <section>
+      You will not see this template. The error is passed to our error handler.
+    </section>
+    
+  `
+}
 export const router = new Router({
     mode:'history',
     routes:[
@@ -22,8 +29,47 @@ export const router = new Router({
                 path:'new-quote',
                 name:'NewQuote',
                 component:NewQuote
-            }
+            },
+            { path: "/error", name: 'error', component: Err },
             ]
         } ,
     ]
 });
+
+// GLobal BEFORE hooks:
+router.beforeEach((to, from, next) => {
+    console.log('Global -- beforeEach - fired')
+
+    // re-route
+    if (to.path === '/hello') {
+        next('/')
+    }
+    // Abort navigation based on some criteria:
+    // else if (to.path === '/user/1') {
+    //   next(false)
+    // }
+    else if (to.path === '/error') {
+        const err = new Error('My Error Message')
+
+        // pass the error to onError() callback.
+        next(err)
+    }
+    else {
+        next()
+    }
+})
+
+// Global beforeResolve
+router.beforeResolve((to, from, next) => {
+    console.log('Global -- beforeResolve - fired.')
+    next()
+})
+
+// GLobal AFTER hooks:
+router.afterEach((to, from) => {
+    // This fires after each route is entered.
+    console.log(`Global -- afterEach - Just moved from '${from.path}' to '${to.path}'`)
+})
+
+
+
